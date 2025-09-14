0988d8ae47c790371431a5eb19164d5c3adf740a
Prachya Sangkharat <gbru@Prachyas-MacBook-Pro.local>
2025-09-14 18:39:06 +0700

    cell01

---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name = "Prachya"
+last_name = "Sangkharat"
+print(first_name, last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Prachya"
+last_name = "Sangkharat"
+whole_name = first_name + " " + last_name
+print(whole_name)
